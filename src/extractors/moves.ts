b0b039d4fb515a3574bb3f89f459d6d0bc0a04df
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-08 15:17:44 +0800

    Moves Extraction Part 2

--- src/extractors/moves.ts
+++ src/extractors/moves.ts
@@ -5,6 +5,7 @@ interface move {
 	id: string;
 	name: string;
 	moveNo: number;
+	description: string;
 	basePower: number;
 	accuracy: number;
 	powerPoints: number;
@@ -24,11 +25,40 @@ for (let lineNo = 0; lineNo < NAMES.length; lineNo++) {
 			id: reduce(NAMES[lineNo].slice(5, -1)),
 			name: NAMES[lineNo].slice(5, -1),
 			moveNo: lineNo - 1,
+			description: '',
 			basePower: -1,
 			accuracy: -1,
 			powerPoints: -1,
 			effectChance: -1
 		});
+	}
+}
+
+const DESCS = readFileSync(
+	import.meta.dirname + '/../../sourcrystal/data/moves/descriptions.asm',
+	'utf-8'
+).split('\n');
+
+for (let lineNo = 0; lineNo < DESCS.length; lineNo++) {
+	if (DESCS[lineNo].endsWith('Description:')) {
+		const id = reduce(DESCS[lineNo].replace('Description:', ''));
+		lineNo++;
+		let description = '';
+		while (DESCS[lineNo].includes('"')) {
+			description += DESCS[lineNo].split('"').at(1)!;
+			if (description.endsWith('-')) {
+				description = description.slice(0, -1);
+				lineNo++;
+				continue;
+			}
+			description += ' ';
+			lineNo++;
+		}
+		description = description.slice(0, -2);
+		const move = moves.find((move) => move.id === id);
+		if (move) {
+			move.description = description;
+		}
 	}
 }
 
