06051e56dbe3c8d46ebd58f5a2a97eef0b674add
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-08 22:00:40 +0800

    Version Extraction

--- src/extractors/index.ts
+++ src/extractors/index.ts
@@ -3,8 +3,10 @@ import items from './items';
 import addresses from './addresses';
 import moves from './moves';
 import pokemon from './pokemon';
+import version from './version';
 
 writeFileSync(import.meta.dirname + '/../lib/data/items.json', JSON.stringify(items));
 writeFileSync(import.meta.dirname + '/../lib/data/addresses.json', JSON.stringify(addresses));
 writeFileSync(import.meta.dirname + '/../lib/data/moves.json', JSON.stringify(moves));
 writeFileSync(import.meta.dirname + '/../lib/data/pokemon.json', JSON.stringify(pokemon));
+writeFileSync(import.meta.dirname + '/../lib/data/version.json', JSON.stringify(version));
