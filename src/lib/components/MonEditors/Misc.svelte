f206f82d5a4122dfae5beeefe62eb5d06e032031
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-10 22:17:06 +0800

    Adds Misc.svelte

--- src/lib/components/MonEditors/Misc.svelte
+++ src/lib/components/MonEditors/Misc.svelte
@@ -1,3 +1,13 @@
 <script lang="ts">
+	import { Heading } from 'flowbite-svelte';
+	import { NumberInput } from '../UI';
 	let { mon = $bindable() } = $props();
 </script>
+
+{#if mon.isEgg}
+	<Heading tag="h6">Egg Hatch Cycles</Heading>
+{:else}
+	<Heading tag="h6">Happiness</Heading>
+{/if}
+<br />
+<NumberInput bind:value={mon.happiness} min={0} max={255} />
