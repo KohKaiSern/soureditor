73ac1b087aa0645ac08925e797dbb38d0866cf2b
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-11 17:53:22 +0800

    Prepare for Bag Reverse Parse

---
+++ src/lib/components/parsers/reverseParseBag.ts
@@ -0,0 +1,7 @@
+import type { BagSlot } from "$lib/types";
+
+export const reverseParseBag = (fileHex: string[], bag: Record<string, BagSlot>): string[] => {
+  return fileHex
+}
+
+export default reverseParseBag
--- src/lib/components/parsers/reverseParseSave.ts
+++ src/lib/components/parsers/reverseParseSave.ts
@@ -1,24 +1,24 @@
-import { parseMonAddresses, reverseParseMon, checksumNB } from '$lib/components/parsers';
+import { parseMonAddresses, reverseParseMon, checksumNB, reverseParseBag } from '$lib/components/parsers';
 import type { Mon, BagSlot } from '$lib/types';
 
 export const reverseParseSave = (
-	fileHex: string[],
-	mons: Mon[][],
-	bag: Record<string, BagSlot>
+  fileHex: string[],
+  mons: Mon[][],
+  bag: Record<string, BagSlot>
 ): string[] => {
-	const monAddresses = parseMonAddresses(fileHex);
-	for (let box = 0; box < 16; box++) {
-		for (let i = 0; i < 20; i++) {
-			if (mons[box][i] === null) {
-				continue;
-			}
-			fileHex = reverseParseMon(fileHex, monAddresses[box][i], mons[box][i]);
-		}
-	}
+  const monAddresses = parseMonAddresses(fileHex);
+  for (let box = 0; box < 16; box++) {
+    for (let i = 0; i < 20; i++) {
+      if (mons[box][i] === null) {
+        continue;
+      }
+      fileHex = reverseParseMon(fileHex, monAddresses[box][i], mons[box][i]);
+    }
+  }
 
-	fileHex = checksumNB(fileHex);
+  fileHex = reverseParseBag(checksumNB(fileHex), bag)
 
-	return fileHex;
+  return fileHex;
 };
 
 export default reverseParseSave;
