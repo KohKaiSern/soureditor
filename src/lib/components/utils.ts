e74f7a3ce3deb83746d2e8da6e7176d2901eaff8
Koh Kai Sern <kaisern.koh@gmail.com>
2026-01-02 16:58:25 +0800

    Fixed Shiny

--- src/lib/components/utils.ts
+++ src/lib/components/utils.ts
@@ -4,7 +4,7 @@ import type { Pokemon } from "$extractors/types";
 import type { BoxMon, PartyMon } from "$parsers/types";
 
 export function isShiny(mon: PartyMon | BoxMon): boolean {
-  return mon.dvs[1] === 0 && mon.dvs[2] === 0 && mon.dvs[3] === 0 && [2, 3, 6, 7, 10, 11, 14, 15].includes(mon.dvs[0])
+  return mon.dvs[1] === 10 && mon.dvs[2] === 10 && mon.dvs[3] === 10 && [2, 3, 6, 7, 10, 11, 14, 15].includes(mon.dvs[0])
 }
 
 export function hpDV(mon: PartyMon | BoxMon): number {
